accd66643947dd8f4f0404c6d38f44ae9a6c52bf
makoto hata <makotohata0429@gmail.com>
2023-12-16 23:56:22 +0900

    add user_edit_page + logout func

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,10 @@
 Rails.application.routes.draw do
   root 'articles#index'
   get '/register', to: 'users#new'
+  get '/settings', to: 'users#edit'
   get '/login', to: 'sessions#new'
   post '/login', to: 'sessions#create'
+  delete '/logout', to: 'sessions#destroy'
   resources :articles
   resources :users
 end

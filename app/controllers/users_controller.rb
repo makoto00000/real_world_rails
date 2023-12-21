001fc5aef30633de61f087d0b8e5a5613edfd111
makoto hata <makotohata0429@gmail.com>
2023-12-21 17:58:25 +0900

    userの編集機能を追加

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,6 +5,8 @@ class UsersController < ApplicationController
 
   def show
     @user = User.find(params[:id])
+    # @articles = Article.paginate(page: params[:page])
+    @articles = @user.articles
   end
 
   def create
@@ -21,8 +23,21 @@ class UsersController < ApplicationController
     @user = User.find(params[:id])
   end
 
+  def update
+    @user = User.find(params[:id])
+    if @user.update(edit_params)
+      redirect_to @user
+    else
+      render 'edit', status: :unprocessable_entity
+    end
+  end
+
   private
     def user_params
       params.require(:user).permit(:name, :email, :password)
+    end
+
+    def edit_params
+      params.require(:user).permit(:image_url, :name, :bio, :email, :password)
     end
 end
